ecdd7b97e5bf3426241320de5d4472fa4c75af46
mirzaeimajd <pe4090731@gmail.com>
2023-05-24 15:19:29 +0430

    meals list styling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
 import { meals } from '../../DummyData';
+import MealItem from './Meal-Item/MealItem';
+import Card from '../UI/Card';
 
 const AvailableMeals = () => {
-    const mealsList = meals.map(meals => <li>{meals.name}</li>)
+    const mealsList = meals.map(meals => <MealItem key={meals.id} name={meals.name} description={meals.description} price={meals.price} />)
     return (
         <section className={classes.meals}>
-            <ul>{mealsList}</ul>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
         </section>
     )
 }
